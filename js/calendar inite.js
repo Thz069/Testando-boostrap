fa30081f3c21065f3748a567267154f37134f368
Thz069 <victor.thoma@gmail.com>
2017-08-16 16:52:47 -0300

    Continuando testes

--- js/calendar inite.js
+++ js/calendar inite.js
@@ -2,30 +2,3 @@
 function init() {
   scheduler.init('scheduler_here',new Date(0),"week");
 }
-function getWidth() {
-  return Math.max(
-    document.body.scrollWidth,
-    document.documentElement.scrollWidth,
-    document.body.offsetWidth,
-    document.documentElement.offsetWidth,
-    document.documentElement.clientWidth
-  );
-}
-
-function getHeight() {
-  return Math.max(
-    document.body.scrollHeight,
-    document.documentElement.scrollHeight,
-    document.body.offsetHeight,
-    document.documentElement.offsetHeight,
-    document.documentElement.clientHeight
-  );
-}
-
-var events = [
-{id:1, text:"Meeting",   start_date:"04/11/2013 14:00",end_date:"04/11/2013 17:00"},
-{id:2, text:"Conference",start_date:"04/15/2013 12:00",end_date:"04/18/2013 19:00"},
-{id:3, text:"Interview", start_date:"04/24/2013 09:00",end_date:"04/24/2013 10:00"}
-];
- 
-scheduler.parse(events, "json");//takes the name and format of the data source
